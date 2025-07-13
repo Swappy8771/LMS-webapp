013f947e5f22e90ff5ccdd13cc85e5e3286e6d1a
swapnil patil <swapnilpatil8771@gmail.com>
2025-07-13 11:56:13 +0530

    Folder Structure with auth setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+       <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello Tailwind!
+      </h1>
+     </div>
+    </>
+  )
+}
+
+export default App
